6e9abe9eea09273b818bb77df5db9dde24c4378e
Bilal Fazlani <bilal_fazlani@external.mckinsey.com>
2018-01-09 10:46:10 +0530

    Initial commit

---
+++ Tests/IoCTests.cs
@@ -0,0 +1,45 @@
+using System;
+using CommandDotNet;
+using CommandDotNet.IoC.MicrosoftDependencyInjection;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Tests
+{
+    public class IoCTests
+    {
+        [Fact]
+        public void CanMicrosfotInjectService()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IService, Service>();
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            AppRunner<ServiceApp> serviceApp =
+                new AppRunner<ServiceApp>().UseMicrosoftDependencyInjection(serviceProvider);
+            
+            serviceApp.Run("Process").Should().Be(4);
+        }
+    }
+    
+    public class ServiceApp
+    {
+        public IService Service { get; set; }
+
+        public int Process()
+        {
+            return Service?.value ?? throw new Exception("Service is not injected");
+        }
+    }
+
+    public interface IService
+    {
+        int value { get; set; }
+    }
+
+    public class Service : IService
+    {
+        public int value { get; set; } = 4;
+    }
+}
