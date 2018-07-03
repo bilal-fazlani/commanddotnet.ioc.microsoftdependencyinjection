d8c8b03f0dbe7b8936aea5c12fd61755c7f2c596
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-07-03 20:17:01 +0530

    Started using UseDependencyResolver method

--- Extension.cs
+++ Extension.cs
@@ -6,7 +6,7 @@ namespace CommandDotNet.IoC.MicrosoftDependencyInjection
     {
         public static AppRunner<T> UseMicrosoftDependencyInjection<T>(this AppRunner<T> appRunner, IServiceProvider serviceProvider) where T :class
         {
-            appRunner.DependencyResolver = new MicrosoftDependencyInjectionResolver(serviceProvider);
+            appRunner.UseDependencyResolver(new MicrosoftDependencyInjectionResolver(serviceProvider));
             return appRunner;
         }
     }
